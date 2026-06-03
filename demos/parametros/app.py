22211fbb4b6d00390151af76da8cd9f0bc975626
MiguelCoro19 <1725110681@utectulancingo.edu.mx>
2026-06-03 14:33:08 +0000

    CREATED configuración del virtual environment

---
+++ demos/parametros/app.py
@@ -0,0 +1,17 @@
+#Muestra paginas y se conecta con base de datos
+import web
+#Permite saber la direccion de la pagina
+urls = (
+    '/', 'Index',
+    '/parametros', 'Parametros'
+)
+#Hace que todo funcione
+app = web.application(urls, globals())
+render = web.template.render('templates')
+
+class Index:
+    def GET(self):
+        return render.index()
+#Ejecuta el servidor
+if __name__ == "__main__":
+    app.run()
