1edd2c037256c11bc1e8b8bb34caeb7318c3de3a
MiguelCoro19 <1725110681@utectulancingo.edu.mx>
2026-06-10 14:51:48 +0000

    CREATED configuración del virtual environment

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -24,10 +24,22 @@ class Calculadora:
         numero_2 = float(formulario['numero_2'])
         resultado = numero_1 + numero_2
 
+        numero_1 = float(formulario['numero_1'])
+        numero_2 = float(formulario['numero_2'])
+        resultado = numero_1 - numero_2
+
+        numero_1 = float(formulario['numero_1'])
+        numero_2 = float(formulario['numero_2'])
+        resultado = numero_1 * numero_2
+
+        numero_1 = float(formulario['numero_1'])
+        numero_2 = float(formulario['numero_2'])
+        resultado = numero_1 / numero_2
+
         print(f"Tipo de dato de numero_1 : {type(numero_1)}")
         print(f"Tipo de dato de numero_2 : {type(numero_2)}")
 
         return render.calculadora(numero_1, numero_2, resultado)
-    
+            
 if __name__ == "__main__":
     app.run()
